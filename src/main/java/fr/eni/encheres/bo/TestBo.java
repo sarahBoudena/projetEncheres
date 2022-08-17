3e76bd0d5fe07d998bbab48df63658e5579cf4c4
sboudena2022 <sboudena2022@105SE32-7Q0TVD3.ad.campus-eni.fr>
2022-08-17 17:27:14 +0200

    Modifs DAL + BO
    Modif BO (ajout du mdp dans un constructeur + MAJ test)
    Créat méthode insert dans la DAL + test OK

--- src/main/java/fr/eni/encheres/bo/TestBo.java
+++ src/main/java/fr/eni/encheres/bo/TestBo.java
@@ -5,7 +5,7 @@ public class TestBo {
 	public static void main(String[] args) {
 		
 		Utilisateur Jose = new Utilisateur (1, "JoseTheKing", "Bachelier", "Jose", "jose.bachelier@eni.com", "0607080910", "rue des gendarmes", "44500", "Blain", 15, false);
-		Utilisateur Jose2 = new Utilisateur ("JoseTheKing", "Bachelier", "Jose", "jose.bachelier@eni.com", "0607080910", "rue des gendarmes", "44500", "Blain", 15, false);
+		Utilisateur Jose2 = new Utilisateur ("JoseTheKing", "Bachelier", "Jose", "jose.bachelier@eni.com", "0607080910", "rue des gendarmes", "44500", "Blain", "Pa$$w0rd", 15, false);
 
 		System.out.println(Jose.toString());
 		System.out.println(Jose2.toString());
