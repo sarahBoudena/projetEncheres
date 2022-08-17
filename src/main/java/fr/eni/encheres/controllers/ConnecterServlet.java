0655d4cf18a8d72d0cebc9cafc811eb6593de270
rbonnand2022 <rbonnand2022@105SE32-9RBVVD3.ad.campus-eni.fr>
2022-08-17 17:25:28 +0200

    cession non testée

--- src/main/java/fr/eni/encheres/controllers/ConnecterServlet.java
+++ src/main/java/fr/eni/encheres/controllers/ConnecterServlet.java
@@ -9,6 +9,7 @@ import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import fr.eni.encheres.bll.BLLException;
 import fr.eni.encheres.bll.utilisateurManager;
@@ -46,7 +47,7 @@ public class ConnecterServlet extends HttpServlet {
 		
 		String email = request.getParameter("email")!=null ?request.getParameter("email"):null ;
 		String password = request.getParameter("password")!=null ? (String)request.getParameter("password"):null ;
-		
+				
 		//		gestion de la case se souvenir par cookie : 
 		Cookie cookie = new Cookie("identite", email);
 		if (request.getParameter("memoriser")!=null) {
@@ -64,6 +65,8 @@ public class ConnecterServlet extends HttpServlet {
 		Utilisateur user =null;
 		try {
 			user = mng.selectById(email, password);
+			HttpSession session = request.getSession();
+			session.setAttribute("user", user);
 			rd =request.getRequestDispatcher("/WEB-INF/jsp/accueil.jsp");
 		} catch (BLLException e) {
 			rd =request.getRequestDispatcher("/WEB-INF/jsp/connection.jsp");
