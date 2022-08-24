8f1b9603e6640b8bffc277fd8f40065291b6adc3
sboudena2022 <sboudena2022@105SE32-7Q0TVD3.ad.campus-eni.fr>
2022-08-24 16:46:36 +0200

    Correction du fichier Context

--- src/main/java/fr/eni/encheres/controllers/AccueillirServlet.java
+++ src/main/java/fr/eni/encheres/controllers/AccueillirServlet.java
@@ -1,6 +1,8 @@
 package fr.eni.encheres.controllers;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -8,6 +10,10 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import fr.eni.encheres.bll.BLLException;
+import fr.eni.encheres.bll.EnchereManager;
+import fr.eni.encheres.bo.ArticleVendu;
 
 /**
  * Servlet implementation class AccueillirServlet
@@ -28,6 +34,15 @@ public class AccueillirServlet extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		List<ArticleVendu> listeEncheresEC = new ArrayList<ArticleVendu>();
+		try {
+			EnchereManager mng = EnchereManager.getInstance();
+			listeEncheresEC = mng.getListeEncheres();
+//			request.setAttribute("listeEncheresEC", listeEncheresEC);
+		} catch (BLLException e) {
+			request.setAttribute("error", e);
+		}
+		
 		
 		RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/jsp/accueil.jsp");
 		rd.forward(request, response);
