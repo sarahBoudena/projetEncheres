ec01c06656759aff460d4b9d169bd9b091890332
sboudena2022 <sboudena2022@105SE32-7Q0TVD3.ad.campus-eni.fr>
2022-08-24 11:54:27 +0200

    MAJ Methode selectEncheresEC
    Construction des objets ArticleVendu, Enchere et Utilisateur

--- src/main/java/fr/eni/encheres/dal/jdbc/EnchereDAOJdbcImpl.java
+++ src/main/java/fr/eni/encheres/dal/jdbc/EnchereDAOJdbcImpl.java
@@ -10,6 +10,7 @@ import java.util.List;
 
 import fr.eni.encheres.bo.ArticleVendu;
 import fr.eni.encheres.bo.Enchere;
+import fr.eni.encheres.bo.Utilisateur;
 import fr.eni.encheres.dal.ConnectionProvider;
 import fr.eni.encheres.dal.DALException;
 import fr.eni.encheres.dal.EnchereDAO;
@@ -32,25 +33,52 @@ public class EnchereDAOJdbcImpl implements EnchereDAO {
 			ResultSet res = pstmt.executeQuery();
 			
 			while (res.next()) {
-				new ArticleVendu(	res.getInt("no_article"),
-									res.getString("nom_article"),
-									res.getString("description"),
-									LocalDateTime.of((res.getDate("date_debut_enchere").toLocalDate()),
-									(res.getTime("date_debut_enchere").toLocalTime())),
-									LocalDateTime.of((res.getDate("date_fin_enchere").toLocalDate()),
-									(res.getTime("date_fin_enchere").toLocalTime())),
-									res.getInt("prix_initial"),
-									res.getInt("no_utilisateur"),
-									res.getInt("no_categorie"),
-									res.getString("image"));
+				 boolean administrateur;
+				 int admin = res.getInt("u.administrateur");
+                if (admin == 0) {
+                    administrateur = false;
+                }
+                else {
+                    administrateur = true;
+                }
+                
+//                encheresEnCours.add(
+                
+				new ArticleVendu(	res.getInt("a.no_article"),
+									res.getString("a.nom_article"),
+									res.getString("a.description"),
+									LocalDateTime.of((res.getDate("a.date_debut_enchere").toLocalDate()),
+									(res.getTime("a.date_debut_enchere").toLocalTime())),
+									LocalDateTime.of((res.getDate("a.date_fin_enchere").toLocalDate()),
+									(res.getTime("a.date_fin_enchere").toLocalTime())),
+									res.getInt("a.prix_initial"),
+									res.getInt("a.no_utilisateur"),
+									res.getInt("a.no_categorie"),
+									res.getString("a.image")
+									//Enchere si !=null
+									//Utilisateur(vendeur
+									);
 				
-				new Enchere(	res.getInt("no_utilisateur"),
-								res.getInt("no_article"),
-								LocalDateTime.of((res.getDate("date_enchere").toLocalDate()),
-								(res.getTime("datet_enchere").toLocalTime())),
-								res.getInt("montant_enchere"));
+				new Enchere(		res.getInt("e.no_utilisateur"),
+									res.getInt("e.no_article"),
+									LocalDateTime.of((res.getDate("e.date_enchere").toLocalDate()),
+									(res.getTime("e.date_enchere").toLocalTime())),
+									res.getInt("e.montant_enchere"));
 				
+
 				
+				new Utilisateur(	res.getInt("u.no_utilisateur"),
+									res.getString("u.pseudo"),
+									res.getString("u.nom"),
+									res.getString("u.prenom"),
+									res.getString("u.email"),
+									res.getString("u.telephone"),
+									res.getString("u.rue"),
+									res.getString("u.code_postal"),
+									res.getString("u.ville"),
+									res.getInt("u.credit"),
+									administrateur);
+//				);
 				
 			}
 		} catch (SQLException e) {
